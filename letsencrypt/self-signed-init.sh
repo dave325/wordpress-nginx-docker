783ca7793ead4c15404489908b722b8c7933f14c
David Dataram <davedataram@gmail.com>
2019-10-07 12:27:07 -0400

    Added new ssl config

--- letsencrypt/self-signed-init.sh
+++ letsencrypt/self-signed-init.sh
@@ -34,6 +34,7 @@ if [ "$#" -ne 1 ]; then
     echo "Usage: $0 FQDN_OR_IP" >&2
     exit 1;
 else
+    echo $1
     FQDN_OR_IP=$1
 fi
 
@@ -44,7 +45,7 @@ fi
 
 # generate and add keys
 openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem \
-    -days 365 -nodes -subj '/CN='${FQDN_OR_IP}''
+    -days 365 -nodes 
 
 mv cert.pem ${CERTS}/live/${FQDN_OR_IP}/fullchain.pem
 mv key.pem ${CERTS}/live/${FQDN_OR_IP}/privkey.pem
@@ -56,4 +57,3 @@ echo "- 40:   ssl_certificate           /etc/letsencrypt/live/${FQDN_OR_IP}/full
 echo "- 41:   ssl_certificate_key       /etc/letsencrypt/live/${FQDN_OR_IP}/privkey.pem;"
 echo "- 42:   #ssl_trusted_certificate   /etc/letsencrypt/live/FQDN_OR_IP/chain.pem; <-- COMMENT OUT OR REMOVE"
 
-exit 0;
